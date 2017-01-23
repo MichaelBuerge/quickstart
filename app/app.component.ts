5dd1162b7f64e6bea7b3058fe20f508a12d8cf1b
Michael Buerge <mib@orino.ch>
2017-01-23 03:35:31 +0100

    Use fetch() and update UI on completion.

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,6 +2,37 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  template: `
+    <h1>Hello {{name}}</h1>
+    <a href=# (click)=fetch()>fetch</a><br>
+    status: <span>{{status}}</span><br>
+    text: <pre>{{text}}</pre><br>
+  `,
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent  {
+  name = 'Angular';
+  status = "-------";
+  text = '-------';
+
+  fetch() {
+    var fetchResult = window['fetch']('');
+    console.log('fetching');
+    this.status = 'fetching';
+    fetchResult.then((res: any) => {
+      console.log('done');
+      this.status = 'done';
+      var textResult = res.text()
+      if (textResult instanceof window['ZoneAwarePromise']) {
+        console.log('textResult is ZoneAwarePromise');
+      } else {
+        console.log(`textRes.constructor patched: ${textResult.constructor.__zone_symbol__thenPatched}`);
+      }
+      textResult.then((textRes: any) => {
+        console.log(textRes.substr(0, 100));
+        this.status = 'got text'
+        this.text = textRes;
+      });
+    });
+  }
+
+}
